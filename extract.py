a5ac0b3b277d25af0d52cac57a9f376d29eff971
magneto <thms.no@gmail.com>
2015-01-23 18:00:14 +0100

    update

--- extract.py
+++ extract.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys
 from exifread import process_file
 
 def extract_exif_data(filename):
     try:
+        print("opening file:", filename)
         f = open(filename, 'rb')
     except IOError:
         print("Could not open file")
         return
 
-    tags = process_file(f)
+    tags = process_file(f, True)
 
-    if tags.has_key("Image GPSInfo"):
+    if "Image GPSInfo" in tags.keys():
         print(tags["Image GPSInfo"])
     else:
         print("no data found")
 
-if __name__ == "main":
+if __name__ == "__main__":
     for arg in sys.argv[1:]:
         extract_exif_data(arg)
