b085cbbed17a2250ad90e5331aa166fbf9eeac99
magneto <thms.no@gmail.com>
2015-01-27 00:50:13 +0100

    update

--- exifData.py
+++ exifData.py
@@ -4,13 +4,26 @@
 import re
 from exifread import process_file
 
+class TagError(Exception):
+    def __init__(self, value="Tag not found"):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
 class ExifData:
     def __init__(self, path, details=False):
-        self.tag = {}
         self.path = path
         self.rePatterns = {
-            'GPS': re.compile('GPS GPS[A-Za-z]+'),
+            'GPS': re.compile('EXIF GPS GPS[A-Za-z]+'),
             'Date': None
+        }
+        self.coordKeys = {
+            'latRef': 'EXIF GPS GPSLatitudeRef',
+            'lat': 'EXIF GPS GPSLatitude',
+            'lonRef': 'EXIF GPS GPSLongitudeRef',
+            'lon': 'EXIF GPS GPSLongitude'
         }
 
         try:
@@ -28,6 +41,11 @@ class ExifData:
             if match:
                 yield match.string
 
+    def __fromDMStoDD(self, DMSCoord):
+        """
+        translate DMS (degrees, minutes, seconds) exifread form to readable DD
+        """
+
     def dump(self, name='GPS'):
         """
         dump any values that could be found in exifread's returned data
@@ -41,3 +59,25 @@ class ExifData:
         return every name that could be passed to dump
         """
         return [k for k in self.rePatterns.keys()]
+
+    def coordinates(self):
+        """
+        return longitude and latitude from DMS notation
+        to a tuple based on following tags' keys defined in self.coordKeys dict:
+        EXIF GPS GPSLatitudeRef
+        EXIF GPS GPSLatitude
+        EXIF GPS GPSLatitudeRef
+        EXIF GPS GPSLongitude
+        """
+        value = lambda t: None if t not in self.tags.keys() else self.tags[t]
+
+        lat = str(value(self.coordKeys['latRef'])) + ' ' +\
+            self.__fromDMStoDD(str(value(self.coordKeys['lat'])))
+        lon = str(value(self.coordKeys['lonRef'])) + ' ' +\
+            self.__fromDMStoDD(str(value(self.coordKeys['lon'])))
+
+
+        print((lat, lon))
+
+        return (lat, lon)
+
